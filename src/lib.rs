b12bd0ff84c5cf0f761b735dd53838351313b039
CharlyCst <charly.castes@epfl.ch>
2024-05-04 14:30:36 +0200

    Playing a bit with Kani
    I tried patching the generated rust code manually to make it executable,
    and then wrote the equivalent code manually and tried comparing the two
    with Kani.
    For the addition with immediate, the code verifies that the manual
    implementation is equivalent to the generated one in ~30s. It is
    basically a look-up in a vector of register, and addition, and a store,
    but uses symbolic values everywhere (immediate, source and destination
    registers, and initial registers value).

---
+++ src/lib.rs
@@ -0,0 +1,71 @@
+#![allow(non_snake_case)]
+
+// ———————————————————————————— Manually written ———————————————————————————— //
+
+#[derive(Default, Clone, PartialEq, Eq, Debug)]
+pub struct Ctx {
+    Xs: [usize; 32],
+}
+
+pub fn mirage_itype(imm: usize, rs1: usize, rd: usize, ctx: &mut Ctx) {
+    if rd == 0 {
+        return;
+    }
+    ctx.Xs[rd] = ctx.Xs[rs1].wrapping_add(imm);
+}
+
+// ——————————————————————————————— Generated ———————————————————————————————— //
+
+fn rX(r: usize, ctx: &mut Ctx) -> usize {
+    match r {
+        b__0 if b__0 == 0b00000 => 0b0000 as usize,
+        _ => ctx.Xs[r],
+    }
+}
+
+fn wX(r: usize, v: usize, ctx: &mut Ctx) {
+    if r != 0b00000 {
+        ctx.Xs[r] = v
+    } else {
+        ()
+    }
+}
+
+pub fn ITYPE(imm: usize, rs1: usize, rd: usize, ctx: &mut Ctx) {
+    let rs1_val = rX(rs1, ctx);
+    let imm_ext: usize = imm as usize;
+    let result = rs1_val.wrapping_add(imm_ext);
+    wX(rd, result, ctx)
+}
+
+// —————————————————————————————————— Kani —————————————————————————————————— //
+
+#[cfg(kani)]
+mod kani {
+    use super::{mirage_itype, Ctx, ITYPE};
+
+    fn any_ctx() -> Ctx {
+        let mut ctx: Ctx = Default::default();
+        for i in 1..32 {
+            ctx.Xs[i] = kani::any();
+        }
+        ctx
+    }
+
+    #[kani::proof]
+    fn itype() {
+        let mut ctx = any_ctx();
+        let imm = kani::any();
+        kani::assume(imm < (1 << 12));
+        let rs1 = kani::any();
+        kani::assume(rs1 < 32);
+        let rd = kani::any();
+        kani::assume(rd < 32);
+
+        let mut mctx = ctx.clone();
+        ITYPE(imm, rs1, rd, &mut ctx);
+        mirage_itype(imm, rs1, rd, &mut mctx);
+
+        assert_eq!(ctx, mctx);
+    }
+}
