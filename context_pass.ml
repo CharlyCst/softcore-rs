22a562b6bfaa9ed75dc7c22605d463f47cefcf94
François Costa <frankost.costa@gmail.com>
2024-10-23 15:14:08 +0200

    WIP: context pass starts looking good here

--- context_pass.ml
+++ context_pass.ml
@@ -6,7 +6,12 @@ open Rust_transform
 
 (* ———————————————————————— VirtContext transformer ————————————————————————— *)
 
-let id_function_modifier (func: rs_fn): rs_fn = func
+
+let id_function_modifier (func: rs_fn): rs_fn = { 
+  func with 
+    args = "sail_ctx" :: func.args;
+    signature = (RsTypId "&mut SailCtx" :: (fst func.signature), snd func.signature)
+}
 
 let virt_context_transform = {
   func = id_function_modifier;
